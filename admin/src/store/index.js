0405110248c7593f4452d7b2379417fc828bd14e
Valentin Alekhin <headbangernetwork@gamail.com>
2020-11-11 18:29:10 +0300

    Create index store

--- admin/src/store/index.js
+++ admin/src/store/index.js
@@ -3,12 +3,36 @@ import Vuex from 'vuex'
 import user from './user'
 import shared from './shared'
 import series from './series'
+import axios from '../axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    series: [],
+    images: [],
+  },
+  mutations: {
+    setImages: (state, payload) => (state.images = payload),
+    setSeries: (state, payload) => (state.series = payload),
+  },
+  actions: {
+    async fetchAllData({ commit }) {
+      commit('setLoading', true)
+      commit('clearError')
+
+      try {
+        const response = await axios.get('/api/ceilnt')
+        const { series, images } = response.data
+
+        commit('setImages', images)
+        commit('setSeries', series)
+      } catch (e) {
+        commit('setError', e)
+      } finally {
+        commit('setLoading', false)
+      }
+    },
+  },
   modules: { user, shared, series },
 })
