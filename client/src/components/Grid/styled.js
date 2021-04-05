6c4d0d9bd6bf4199395e8145490582050ffd60d0
Alekhin Valentin <headbangernetwork@gmail.com>
2021-04-05 20:26:52 +0300

    Add Grid

--- client/src/components/Grid/styled.js
+++ client/src/components/Grid/styled.js
@@ -1,5 +1,23 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div``
+import { respondTo } from 'style/respondTo'
 
-export const Coll = styled.div``
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-column-gap: 1rem;
+
+  ${respondTo.sm`
+    grid-template-columns: 1fr 1fr;
+  `}
+
+  ${respondTo.xl`
+    grid-template-columns: 1fr 1fr 1fr;
+  `}
+`
+
+export const ColumnItem = styled.div`
+  margin-bottom: 1rem;
+`
+
+export const Column = styled.div``
