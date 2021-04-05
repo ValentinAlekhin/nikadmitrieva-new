82533c07a5331c34de7c6ee57aae4783f2a542aa
Alekhin Valentin <headbangernetwork@gmail.com>
2021-04-05 14:55:32 +0300

    Add react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
+import { Switch, Route, useLocation } from 'react-router-dom'
+
 import GlobalStyles from './style/GlobalStyles'
 
 import Header from './layout/Header/Header'
 
 const App = () => {
+  const location = useLocation()
+
   return (
     <>
       <GlobalStyles />
       <Header />
+
+      <Switch location={location} key={location.pathname}></Switch>
     </>
   )
 }
