436f49455462577ead4ea8066e041c0bb28e6842
Alekhin Valentin <headbangernetwork@gmail.com>
2021-04-06 15:16:15 +0300

    Add npm pack grid

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,26 +1,37 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
 
 import { ContentContext } from 'context/ContentState'
 
 import MainContainer from 'shared/MainContainer/MainContainer'
-import Grid from 'components/Grid/Grid'
 import SeriesCard from 'components/SeriesCard/SeriesCard'
 
 import { Wrapper } from './styled'
 
+import { breakpoints } from 'style/variables'
+
+const gridBreakpoints = {
+  [breakpoints.xs]: 1,
+  [breakpoints.sm]: 2,
+  [breakpoints.lg]: 3,
+  [breakpoints.xl]: 4,
+}
+
 const Home = () => {
   const { getSeries } = useContext(ContentContext)
   const gridItems = getSeries().map(({ title, titleImage, _id }, i) => (
-    <Link to={`/series/${_id}`}>
-      <SeriesCard key={i} title={title} titleImage={titleImage} />
+    <Link key={i} to={`/series/${_id}`}>
+      <SeriesCard title={title} titleImage={titleImage} />
     </Link>
   ))
 
   return (
     <MainContainer>
       <Wrapper>
-        <Grid items={gridItems} />
+        <ResponsiveMasonry columnsCountBreakPoints={gridBreakpoints}>
+          <Masonry gutter="20px">{gridItems}</Masonry>
+        </ResponsiveMasonry>
       </Wrapper>
     </MainContainer>
   )
