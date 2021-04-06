82d06b97dd16c44d8851ecfa3e2780d71cd315d4
Alekhin Valentin <headbangernetwork@gmail.com>
2021-04-06 15:30:15 +0300

    Add Grid to Series page

--- client/src/pages/Series/Series.jsx
+++ client/src/pages/Series/Series.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+
+import { ContentContext } from 'context/ContentState'
+
+import MainContainer from 'shared/MainContainer/MainContainer'
+import Grid from 'components/Grid/Grid'
+import SeriesCard from 'components/SeriesCard/SeriesCard'
+
+import { Wrapper } from './styled'
 
 const Series = () => {
-  return <div>Series</div>
+  const { id } = useParams()
+  const { getSeriesImages } = useContext(ContentContext)
+  const gridItems = getSeriesImages(id).map((img, i) => (
+    <SeriesCard key={i} image={img} />
+  ))
+
+  return (
+    <MainContainer>
+      <Wrapper>
+        <Grid>{gridItems}</Grid>
+      </Wrapper>
+    </MainContainer>
+  )
 }
 
 export default Series
