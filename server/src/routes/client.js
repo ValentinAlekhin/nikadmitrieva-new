17caba52cac7ac78bd2ff099b6e80275b286e7ca
Alekhin Valentin <headbangernetwork@gmail.com>
2021-04-06 16:00:12 +0300

    Add clicks handlers routes

--- server/src/routes/client.js
+++ server/src/routes/client.js
@@ -18,4 +18,30 @@ router.get('/', async (req, res) => {
   }
 })
 
+router.get('/view/series/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+
+    await Series.findByIdAndUpdate(id, { $inc: { clicks: 1 } })
+
+    res.status(200)
+  } catch (e) {
+    res.status(500).json({ message: e })
+    throw new Error(e)
+  }
+})
+
+router.get('/view/image/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+
+    await Image.findByIdAndUpdate(id, { $inc: { clicks: 1 } })
+
+    res.status(200)
+  } catch (e) {
+    res.status(500).json({ message: e })
+    throw new Error(e)
+  }
+})
+
 module.exports = router
