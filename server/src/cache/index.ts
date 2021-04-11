a4e884a116c379cf2667c5a634ab924ccc475e00
Alekhin Valentin <headbangernetwork@gmail.com>
2021-04-11 14:28:14 +0300

    Add cache logic

---
+++ server/src/cache/index.ts
@@ -0,0 +1,47 @@
+import NodeCache from 'node-cache'
+
+class Cache {
+  private readonly cache: NodeCache
+
+  constructor(ttlSeconds: number) {
+    this.cache = new NodeCache({
+      stdTTL: ttlSeconds,
+      checkperiod: ttlSeconds * 0.2,
+      useClones: false,
+    })
+  }
+
+  async get(key: string, storeFunction: () => Promise<any>) {
+    const value = this.cache.get(key)
+    if (value) {
+      return value
+    }
+
+    const result = await storeFunction()
+    this.cache.set(key, result)
+    return result
+  }
+
+  del(keys: string) {
+    this.cache.del(keys)
+  }
+
+  delStartWith(startStr = '') {
+    if (!startStr) {
+      return
+    }
+
+    const keys = this.cache.keys()
+    for (const key of keys) {
+      if (key.indexOf(startStr) === 0) {
+        this.del(key)
+      }
+    }
+  }
+
+  flush() {
+    this.cache.flushAll()
+  }
+}
+
+export default Cache
