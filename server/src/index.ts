b39b47a911fc8b84e5fc8da2189e8ee37da1da3d
Alekhin Valentin <headbangernetwork@gmail.com>
2021-04-11 17:48:39 +0300

    Add gzip compression

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import path from 'path'
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import compression from 'compression'
 
 import authRoute from './routes/auth'
 import clientRoute from './routes/client'
@@ -17,12 +18,13 @@ const PORT = process.env.PORT || 5000
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
+app.use(compression())
 
 app.use('/api/auth', authRoute)
 app.use('/api/client', clientRoute)
 app.use('/api/admin', adminRoute)
 
-app.use('/data', express.static(path.join(__dirname, '../data')))
+app.use('/images', express.static(path.join(__dirname, '../data/images')))
 
 async function start() {
   try {
