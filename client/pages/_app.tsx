368705fa9b11b741556b121dafae5a44a920b361
Alekhin Valentin <headbangernetwork@gmail.com>
2021-04-14 13:48:33 +0300

    Add local fonts

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from 'next/app'
 
 import Layout from '@components/Layout/Layout'
-import GlobalStyles from '@styles/ GlobalStyles'
+import GlobalStyles from '@styles/GlobalStyles'
+import GlobalFonts from '@styles/GlobalFonts'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <GlobalFonts />
       <GlobalStyles />
       <Layout>
         <Component {...pageProps} />
--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -48,11 +48,6 @@ class MyDocument extends Document {
             content="Ника Дмитриева. Художник и фотограф. На сайте собраны все лучшие работы. Приятного просмотра!"
             key="og:title"
           />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
