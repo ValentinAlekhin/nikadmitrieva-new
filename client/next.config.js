b639a6756a322155dc33d55bdf45b4ffe7632a89
Alekhin Valentin <headbangernetwork@gmail.com>
2021-04-14 11:18:47 +0300

    Add proxy to client

--- client/next.config.js
+++ client/next.config.js
@@ -7,4 +7,12 @@ module.exports = {
 
     return config
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5000/:path*', // Proxy to Backend
+      },
+    ]
+  },
 }
