6974107cadb92de554980a15616b635d60aa24e1
Valentin Alekhin <headbangernetwork@gmail.com>
2022-02-11 21:11:57 +0300

    Add strapi

--- server/src/libs/fileExists.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs'
-
-const fileExits = (path: string): Promise<boolean> =>
-  new Promise(resolve =>
-    fs.access(path, err => (err ? resolve(false) : resolve(true)))
-  )
-
-export default fileExits
